f4264b8157b81661ea634eef122d8b1a190359a7
brijesh <pantbrijesh20@gmail.com>
2022-01-26 20:18:49 +0530

    Added set up for multimodule project

---
+++ module-1/src/main/java/com/brij/Application.java
@@ -0,0 +1,7 @@
+package com.brij;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.printf("Hello from App %s current time : %s", Application.class, DateUtil.getCurrentDate());
+    }
+}
---
+++ module-2/src/main/java/com/brij/DateUtil.java
@@ -0,0 +1,9 @@
+package com.brij;
+
+import java.util.Date;
+
+public class DateUtil {
+    public static Date getCurrentDate() {
+        return new Date();
+    }
+}
